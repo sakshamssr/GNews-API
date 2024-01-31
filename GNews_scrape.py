d3137e20d0e7e4359f8cd336ab292a531ce795ca
Saksham Singh <62788658+sakshamssr@users.noreply.github.com>
2024-01-31 23:19:00 +0530

    Update GNews_scrape.py

--- GNews_scrape.py
+++ GNews_scrape.py
@@ -8,7 +8,7 @@ def scrape(inp):
     l=[]
     store={}
 
-    URL = "https://news.google.com/search?q="+inp
+    URL = "https://news.google.com/search?q="+str(query)
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, "html.parser")
     name = soup.find_all(class_="JtKRv")
@@ -16,9 +16,16 @@ def scrape(inp):
     time=soup.find_all(class_="hvbAAd")
     link=soup.find_all(class_="WwrzSb")
 
+    #print("Name:",name)
+    #print(image)
+    
+    #print("Time:",time)
+    #print(link)
+
     for i in range(0,len(name)):
         try:
-            title=str(name[i]).split('tabindex="0">')[1].split("</h4>")[0]
+            title=str(name[i]).split('target="_blank">')[1].split("</a>")[0]
+            #print(title)
             imagelink=str(image[i]).split('src="')[1].split('" src')[0]
             upload=time[i].text
             articleLink=str(link[i]).split('href="')[1].split('" jslog')[0]
@@ -26,7 +33,7 @@ def scrape(inp):
         except:
             print("That's Enough!")
             break
-    
+    #print(store)
     return store
 
 app = FastAPI()
